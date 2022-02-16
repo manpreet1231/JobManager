972dda495716d7cc3e14720bbd5400729f7b6e7a
manpreet1231 <96197943+manpreet1231@users.noreply.github.com>
2022-02-16 23:10:39 +0500

    Create Job model and generate getters setters Create IJobDataStore service Create JobDataStoreLocalJson service
    Create Job model and generate getters setters of job id, name and description
    Create IJobDataStore service and generate Tasks for job GetJobs, GetJob by job id, AddJob, UpdateJob and DeleteJob
    Create JobDataStoreLocalJson service , inherit with IJobDataStore service and get Jobs list

--- JobManager/Models/Job.cs
+++ JobManager/Models/Job.cs
@@ -6,6 +6,9 @@ namespace JobManager.Models
 {
     public class Job
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
     }
 }
--- JobManager/Services/IJobDataStore.cs
+++ JobManager/Services/IJobDataStore.cs
@@ -8,6 +8,10 @@ namespace JobManager.Services
 {
     public interface IJobDataStore<T>
     {
-      
+        Task<IEnumerable<Job>> GetJobs();
+        Task<Job> GetJob(string jobId);
+        Task AddJob(Job job);
+        Task UpdateJob(Job job);
+        Task DeleteJob(Job job);
     }
 }
