99bef21395477e891147c3d06b2e21e83474f510
manpreet1231 <96197943+manpreet1231@users.noreply.github.com>
2022-02-16 19:42:34 +0500

    Create JobManager android application using Xamarin forms and C#
    Create JobManager android application using Xamarin forms and C#.
    Delete built in created files from Models, Services, ViewModels and Views folders

---
+++ JobManager/AppShell.xaml.cs
@@ -0,0 +1,11 @@
+﻿
+namespace JobManager
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
