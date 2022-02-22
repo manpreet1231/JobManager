5aa301f02f89d200889cb4d27882b62d5fa46aec
manpreet1231 <96197943+manpreet1231@users.noreply.github.com>
2022-02-23 00:07:45 +0500

    JobsController.cs added MVC Controller with read/write actions.
    JobsController.cs added MVC Controller with read/write actions.

---
+++ JobManager.API/Controllers/JobsController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobManager.API.Controllers
+{
+    public class JobsController : Controller
+    {
+        // GET: JobsController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: JobsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: JobsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: JobsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: JobsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: JobsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: JobsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: JobsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
