a25a73ef6dec7bbb304dd38b5d3e23ec8d86d94b
manpreet1231 <96197943+manpreet1231@users.noreply.github.com>
2022-02-25 01:13:17 +0500

    Add new project that name is CreateWebApi in this project get jobs list
    Add new project that name is CreateWebApi in this project get jobs list

---
+++ CreateWebApi/CreateWebApi/Controllers/JobsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobManager.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class JobsController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Job> Get()
+        {
+            return GetDefaultJobs();
+        }
+        private List<Job> GetDefaultJobs()
+        {
+
+            var jobs = new List<Job>() {
+                new Job { Id = 1, Name = "Job A API" , Description = "This is api a."},
+                new Job { Id = 2, Name = "Job B API" , Description = "This is api b."},
+                new Job { Id = 3, Name = "Job C API" , Description = "This is api c."},
+                new Job { Id = 4, Name = "Job D API" , Description = "This is api d."}
+
+            };
+            return jobs;
+        }
+    }
+}
---
+++ CreateWebApi/CreateWebApi/Job.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace JobManager.API
+{
+    public class Job
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
